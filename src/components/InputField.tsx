c7f922e6a3e52a808ddabf237b378a0511c4c36f
Shobhit <121398721+orpic@users.noreply.github.com>
2023-06-25 22:23:50 +0530

    Input field

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,5 +1,14 @@
+import "./styles.css";
+
 const InputField = () => {
-  return <div></div>;
+  return (
+    <form className="input">
+      <input type="input" placeholder="Enter a task" className="input__box" />
+      <button className="input_submit" type="submit">
+        Go
+      </button>
+    </form>
+  );
 };
 
 export default InputField;
