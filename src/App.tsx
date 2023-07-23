ed7028810f0e0c6662b6a175e2af0554e66cd227
shobhit <shobhitumrao5@gmail.com>
2023-07-23 23:40:21 +0530

    adding interface in input element

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+
+  console.log(todo);
+
   return (
     <div className="App">
       <span className="heading">Todo App</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,23 @@
+import React from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }: Props) => {
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box" />
+      <input
+        value={todo}
+        onChange={(e) => {
+          setTodo(e.target.value);
+        }}
+        type="input"
+        placeholder="Enter a task"
+        className="input__box"
+      />
       <button className="input_submit" type="submit">
         Go
       </button>
