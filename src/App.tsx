6d21d6fd035c5382794552b19e305a5377a8ee44
shobhit <shobhitumrao5@gmail.com>
2023-07-23 23:54:40 +0530

    form synthetic event type
    add todo in todos list , create types for default handle form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
 import { Todo } from "./model";
@@ -7,10 +7,16 @@ const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+    setTodo("");
+  };
+
   return (
     <div className="App">
       <span className="heading">Todo App</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 };
