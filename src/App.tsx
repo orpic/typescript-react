ffa2bc93351a8d9ed49c134f8efc6cc0dbb4a447
shobhit <shobhitumrao5@gmail.com>
2023-07-25 21:51:20 +0530

    styles todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import "./App.css";
-import InputField from "./components/InputField";
 import { Todo } from "./model";
+import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
@@ -13,10 +14,13 @@ const App: React.FC = () => {
     setTodo("");
   };
 
+  console.log(todo, todos);
+
   return (
     <div className="App">
       <span className="heading">Todo App</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
